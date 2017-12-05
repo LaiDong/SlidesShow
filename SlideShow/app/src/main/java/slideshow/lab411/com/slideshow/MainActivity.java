6239bca69b7abfdb5689683a9511f20c4622d417
manhterry93 <manhterry93@gmail.com>
2017-12-05 23:19:21 +0700

    Init project
    First init

---
+++ SlideShow/app/src/main/java/slideshow/lab411/com/slideshow/MainActivity.java
@@ -0,0 +1,13 @@
+package slideshow.lab411.com.slideshow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
